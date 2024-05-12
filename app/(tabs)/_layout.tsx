ed132862da62c5657bfa9ecda2bde1a58e4fa25c
Matt Jardine <matt@noteflight.com>
2024-05-12 15:58:13 -0700

    logos

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,6 @@
-import { Tabs } from 'expo-router';
 import HomeScreen from './index';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
-
   return (<HomeScreen />)
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -43,8 +43,8 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/bad_cat_logo_partial.png')}
+          style={styles.logo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
@@ -78,9 +78,9 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
+  logo: {
+    height: 150,
+    width: 180,
     bottom: 0,
     left: 0,
     position: 'absolute',
